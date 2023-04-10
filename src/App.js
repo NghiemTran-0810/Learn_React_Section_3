c4deaec25b21456d0fdb7d5022433dd64b963556
Shuu09 <nghiemtrang0810@gmail.com>
2023-04-10 23:25:33 +0700

    create Foo component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
+import Foo from './components/Foo';
 const App = () => {
   const count = useSelector(state => state.counter.count);
   const dispatch = useDispatch();
 
   return (
     <div className="App">
-      <header className="App-header">
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -16,7 +18,10 @@ const App = () => {
         <div>Count = {count}</div>
         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-      </header>
+      </header> */}
+
+      {/* Components */}
+      <Foo />
     </div>
   );
 }
---
+++ src/components/Foo.js
@@ -0,0 +1,11 @@
+import React from "react";
+class Foo extends React.Component {
+    // JSX
+    render() {
+        return (
+            <p>my first component in ReactJS with random number {Math.random()}</p>
+        )
+    }
+}
+
+export default Foo;
