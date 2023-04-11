12e34f08a48db3e53b0d05b8228f5ab892440f9a
Shuu09 <nghiemtrang0810@gmail.com>
2023-04-11 09:14:19 +0700

    learn state

---
+++ src/components/State_example.js
@@ -0,0 +1,17 @@
+import React from "react";
+class StateExample extends React.Component {
+    // State 
+    state = {
+        name: 'shuu',
+        age: 26,
+        address: '123 ma_may street'
+    }
+    // JSX
+    render() {
+        return (
+            <p>{this.state.name} live in {this.state.address}</p>
+        )
+    }
+}
+
+export default StateExample;
